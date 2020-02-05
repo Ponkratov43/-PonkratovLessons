01d4f208420d7251726244dc1987b07b419f9544
Ponkratov <ponkratov_2000@mail.ru>
2020-02-05 03:37:20 +0300

    homework03

--- src/by/belhard/j20/ponkratov/homework/homework03/Task1.java
+++ src/by/belhard/j20/ponkratov/homework/homework03/Task1.java
@@ -3,25 +3,22 @@ package by.belhard.j20.ponkratov.homework.homework03;
 public class Task1 {
     public static void main(String[] args) {
 
-        int[] mass = new int[101];
-        for (int i = 0; i < mass.length; i++) {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
             if (i % 3 == 0 && i % 7 == 0) {
-                mass[i] += i;
+                arr[i] +=i;
                 System.out.print(i + " ");
                 continue;
             }
         }
         System.out.println();
-        for (int i = 0; i < mass.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if ((i % 5 == 0 && i % 7 != 0) || (i % 7 == 0 && i % 5 != 0)) {
-                mass[i] += i;
+               arr[i] += i;
                 System.out.print(i + " ");
                 continue;
             }
         }
         System.out.println();
-
-        byte[][] arrayBytes = {{1, 2, 3}, {4, 5, 6}};
-        System.out.println(arrayBytes[1][1]);
     }
 }
